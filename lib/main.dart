a7a8881fa5ecbdfe3054e6fa816d8cef9f7ad75b
Hamza-ye <7amza.it@gmail.com>
2020-08-11 00:30:36 +0300

    Add the Auth user Stream

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:cofe_example_firebase/models/user.dart';
 import 'package:cofe_example_firebase/screens/wrapper.dart';
+import 'package:cofe_example_firebase/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,8 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,13 @@
+import 'package:cofe_example_firebase/models/user.dart';
 import 'package:cofe_example_firebase/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    print('user uid: ${user?.uid}');
     // return either Auth or home
     return Authenticate();
   }
