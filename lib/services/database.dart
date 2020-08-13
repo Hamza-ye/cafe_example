b04756d0729a5fc1dcb72c36f596de510774f1cd
Hamza-ye <7amza.it@gmail.com>
2020-08-14 01:30:13 +0300

    Add firebase database service class

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cofe_example_firebase/models/brew.dart';
+import 'package:cofe_example_firebase/models/user.dart';
 
 class DatabaseService {
   final String uid;
@@ -21,5 +22,18 @@ class DatabaseService {
   // get brews stream
   Stream<List<Brew>> get brews {
     return reference.snapshots().map(_brewListFromSnapshot);
+  }
+
+  // userData from userdata snapshots stream
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) => UserData(
+        uid: uid,
+        name: snapshot.data['name'] ?? '',
+        sugars: snapshot.data['sugars'] ?? '0',
+        strength: snapshot.data['strength'] ?? 100,
+      );
+
+  // get user data
+  Stream<UserData> get userData {
+    return reference.document(uid).snapshots().map(_userDataFromSnapshot);
   }
 }
